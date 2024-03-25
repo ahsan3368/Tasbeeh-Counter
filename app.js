b6efd9cf668d44aa974f7096f7ce88ee5ec0b65e
Ahsan <ahsantariq1401@gmail.com>
2024-03-26 00:22:29 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+// first 
+
+
+
+let value1 = document.getElementById("value1");
+let count1 = 0;
+function inc1() {
+    count1 ++;
+    value1.innerText = count1;
+}
+function reset1() {
+    count1 = 0;
+    value1.innerText = count1;
+}
+
+
+// second
+
+
+let value2 =document.getElementById("value2");
+let count2 = 0;
+function inc2() {
+    count2 ++;
+    value2.innerText = count2;
+}
+function reset2() {
+    count2 = 0;
+    value2.innerText = count2;
+}
+
+
+// third
+
+
+
+
+let value3 =document.getElementById("value3");
+let count3 = 0;
+function inc3() {
+    count3 ++;
+    value3.innerText = count3;
+}
+function reset3() {
+    count3 = 0;
+    value3.innerText = count3;
+}
+
